a81d92b08a59e5c546b881c6444e98b094c32504
Aleksandr.Fedorov <aleksandr.fedorov@billing.ru>
2018-01-23 01:26:34 +0300

    fix + small dataset

--- accident.py
+++ accident.py
@@ -74,7 +74,7 @@ def build_model():
     }
 
     # Define a lstm cell with tensorflow
-    lstm_cell = tf.nn.rnn_cell.LSTMCell(n_hidden,n_hidden,initializer= tf.random_normal_initializer(mean=0.0,stddev=0.01),use_peepholes = True,state_is_tuple = False)
+    lstm_cell = tf.nn.rnn_cell.LSTMCell(n_hidden,n_hidden,initializer= tf.random_normal_initializer(mean=0.0,stddev=0.01),state_is_tuple = False)
     # using dropout in output of LSTM
     lstm_cell_dropout = tf.nn.rnn_cell.DropoutWrapper(lstm_cell,output_keep_prob=1 - keep[0])
     # init LSTM parameters
@@ -94,16 +94,16 @@ def build_model():
         n_object = tf.reshape(X[1:n_detection,:,:], [-1, n_input]) # (n_steps*batch_size, n_input)
         n_object = tf.matmul(n_object, weights['em_obj']) + biases['em_obj'] # (n x b) x h
         n_object = tf.reshape(n_object,[n_detection-1,batch_size,n_att_hidden]) # n-1 x b x h
-        n_object = tf.mul(n_object,tf.expand_dims(zeros_object[i],2))
+        n_object = tf.multiply(n_object,tf.expand_dims(zeros_object[i],2))
 
         # object attention
         brcst_w = tf.tile(tf.expand_dims(weights['att_w'], 0), [n_detection-1,1,1]) # n x h x 1
         image_part = tf.batch_matmul(n_object, tf.tile(tf.expand_dims(weights['att_ua'], 0), [n_detection-1,1,1])) + biases['att_ba'] # n x b x h
         e = tf.tanh(tf.matmul(h_prev,weights['att_wa'])+image_part) # n x b x h
         # the probability of each object
-        alphas = tf.mul(tf.nn.softmax(tf.reduce_sum(tf.batch_matmul(e,brcst_w),2),0),zeros_object[i])
+        alphas = tf.multiply(tf.nn.softmax(tf.reduce_sum(tf.batch_matmul(e,brcst_w),2),0),zeros_object[i])
         # weighting sum
-        attention_list = tf.mul(tf.expand_dims(alphas,2),n_object)
+        attention_list = tf.multiply(tf.expand_dims(alphas,2),n_object)
         attention = tf.reduce_sum(attention_list,0) # b x h
         # concat frame & object
         fusion = tf.concat(1,[image,attention])
@@ -127,11 +127,11 @@ def build_model():
             all_alphas = tf.concat(0,[all_alphas, temp_alphas])
 
         # positive example (exp_loss)
-        pos_loss = -tf.mul(tf.exp(-(n_frames-i-1)/20.0),-tf.nn.softmax_cross_entropy_with_logits(pred, y))
+        pos_loss = -tf.multiply(tf.exp(-(n_frames-i-1)/20.0),-tf.nn.softmax_cross_entropy_with_logits(pred, y))
         # negative example
         neg_loss = tf.nn.softmax_cross_entropy_with_logits(pred, y) # Softmax loss
 
-        temp_loss = tf.reduce_mean(tf.add(tf.mul(pos_loss,y[:,1]),tf.mul(neg_loss,y[:,0])))
+        temp_loss = tf.reduce_mean(tf.add(tf.multiply(pos_loss,y[:,1]),tf.multiply(neg_loss,y[:,0])))
         #loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
         loss = tf.add(loss, temp_loss)
         
